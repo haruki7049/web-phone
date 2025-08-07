bbb9f9505e648a8c7bd78d3dd21784e6c05fe3fd
haruki7049 <tontonkirikiri@gmail.com>
2025-08-07 10:08:50 +0900

    Update

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,12 +1,30 @@
 use clap::Parser;
+use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
 use std::net::{Ipv4Addr, SocketAddr, TcpListener, TcpStream};
+use std::path::PathBuf;
+use std::sync::{LazyLock, Mutex};
 use std::thread::spawn;
 use tungstenite::{Message, accept};
 
+static DEFAULT_CONFIG_PATH: LazyLock<Mutex<PathBuf>> = LazyLock::new(|| {
+    let proj_dirs = ProjectDirs::from("dev", "haruki7049", "web-phone-daemon")
+        .expect("Failed to search ProjectDirs for dev.haruki7049.web-phone-daemon");
+    let mut config_path: PathBuf = proj_dirs.config_dir().to_path_buf();
+    let filename: &str = "config.toml";
+
+    config_path.push(filename);
+    Mutex::new(config_path)
+});
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: CLIArgs = CLIArgs::parse();
-    let address: String = format!("{}:{}", args.ip, args.port);
+    let config: Configuration = confy::load_path(&args.config_path).unwrap_or_else(|_| {
+        eprintln!("Running web-phone-daemon with default Configuration...");
+        Configuration::default()
+    });
 
+    let address: String = format!("{}:{}", config.ip, config.port);
     let server = TcpListener::bind(&address)?;
     eprintln!("Running on ws://{}", &address);
     eprintln!("Use Ctrl-C to stop this program");
@@ -53,9 +71,21 @@ fn save_message(text: &str, addr: SocketAddr) -> Result<(), Box<dyn std::error::
 
 #[derive(Parser)]
 struct CLIArgs {
-    #[arg(short, long)]
-    ip: Ipv4Addr,
+    #[arg(short, long, default_value = DEFAULT_CONFIG_PATH.lock().unwrap().display().to_string())]
+    config_path: PathBuf,
+}
 
-    #[arg(short, long)]
+#[derive(Debug, Serialize, Deserialize)]
+struct Configuration {
+    ip: Ipv4Addr,
     port: u16,
+}
+
+impl std::default::Default for Configuration {
+    fn default() -> Self {
+        Self {
+            ip: Ipv4Addr::new(127, 0, 0, 1),
+            port: 15000,
+        }
+    }
 }
